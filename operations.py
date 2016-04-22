de96dcb3908c00787f01f31661af3939133cf8c5
tapa8728 <tapa8728@colorado.edu>
2016-04-21 18:27:54 -0600

    operations.py - month digits replaced by calendar month name

--- operations.py
+++ operations.py
@@ -1,9 +1,15 @@
-import operation
+import calendar
 
 f  = open("drinking_answers.txt", "r")
 lines  = f.readlines()
 
 for each in lines[1:]:
 	m = each.split(",")
-	print m
+	gfgid = m[0]
+	year = m[1]
+	month= calendar.month_abbr[int(m[2])]
+	day = m[3]
+	had = m[4]
+	num = m[5].replace("\n", "")
+	print "year:",year,"month:", month, "day:",day,"had?:", had," #",num
 	break
