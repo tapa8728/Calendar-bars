1661f38d66ca6d9c9969c305f809a3a8998f7b90
tapa8728 <tapa8728@colorado.edu>
2016-04-25 08:16:40 -0600

    inital JSON dumped

--- operations.py
+++ operations.py
@@ -1,4 +1,4 @@
-import calendar
+import calendar, json
 
 f  = open("drinking_answers.txt", "r")
 lines  = f.readlines()
@@ -17,27 +17,52 @@ for each in lines[1:]:
 	print "gender:",gender,"year:",year,"month:", month, "day:",day,"had?:", had," #",num
 	if key not in dic:
 		dic[key] = {}
-		dic[key]["gender"] = "null"
-		dic[key]["no"] = 0
-		dic[key]["yes"] = 0
-		dic[key]["numyes"] = 0
-		dic[key]["total"] = 0
-		if had == "No":
-			dic[key]["no"] = dic[key]["no"] + 1
-		elif had == "Yes":
-			dic[key]["yes"] = dic[key]["yes"] + 1
-			dic[key]["numyes"] = dic[key]["numyes"] + int(num)
-		dic[key]["total"] = dic[key]["total"] + 1
-		dic[key]["gender"] = gender
+		dic[key]["female"] = {}
+		dic[key]["male"] ={}
+
+		dic[key]["female"]["no"] = 0
+		dic[key]["female"]["yes"] = 0
+		dic[key]["female"]["numyes"] = 0
+		dic[key]["female"]["total"] = 0
+
+		dic[key]["male"]["no"] = 0
+		dic[key]["male"]["yes"] = 0
+		dic[key]["male"]["numyes"] = 0
+		dic[key]["male"]["total"] = 0
+		if gender == "male":
+			if had == "No":
+				dic[key]["male"]["no"] = dic[key]["male"]["no"] + 1
+			elif had == "Yes":
+				dic[key]["male"]["yes"] = dic[key]["male"]["yes"] + 1
+				dic[key]["male"]["numyes"] = dic[key]["male"]["numyes"] + int(num)
+			dic[key]["male"]["total"] = dic[key]["male"]["total"] + 1
+		elif gender == "female":
+			if had == "No":
+				dic[key]["female"]["no"] = dic[key]["female"]["no"] + 1
+			elif had == "Yes":
+				dic[key]["female"]["yes"] = dic[key]["female"]["yes"] + 1
+				dic[key]["female"]["numyes"] = dic[key]["female"]["numyes"] + int(num)
+			dic[key]["female"]["total"] = dic[key]["female"]["total"] + 1
+			
 	else:
-		if had == "No":
-			dic[key]["no"] = dic[key]["no"] + 1
-		elif had == "Yes":
-			dic[key]["yes"] = dic[key]["yes"] + 1
-			dic[key]["numyes"] = dic[key]["numyes"] + int(num)
-		dic[key]["total"] = dic[key]["total"] + 1
-		dic[key]["gender"] = gender
-	if dic[key]["total"] == 10:
+		if gender == "male":
+			if had == "No":
+				dic[key]["male"]["no"] = dic[key]["male"]["no"] + 1
+			elif had == "Yes":
+				dic[key]["male"]["yes"] = dic[key]["male"]["yes"] + 1
+				dic[key]["male"]["numyes"] = dic[key]["male"]["numyes"] + int(num)
+			dic[key]["male"]["total"] = dic[key]["male"]["total"] + 1
+		elif gender == "female":
+			if had == "No":
+				dic[key]["female"]["no"] = dic[key]["female"]["no"] + 1
+			elif had == "Yes":
+				dic[key]["female"]["yes"] = dic[key]["female"]["yes"] + 1
+				dic[key]["female"]["numyes"] = dic[key]["female"]["numyes"] + int(num)
+			dic[key]["female"]["total"] = dic[key]["female"]["total"] + 1
+
+	if dic[key]["female"]["total"] == 10:
 		break
 
 print dic
+with open('drinking.json', 'w') as outfile:
+    json.dump(dic, outfile)
