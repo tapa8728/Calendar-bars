dd57bef463256a2fbdb5deaa0cf0f37dc0184c44
tapa8728 <tapa8728@colorado.edu>
2016-04-25 07:54:10 -0600

    Operations.py - dictionary structure created

--- operations.py
+++ operations.py
@@ -2,7 +2,7 @@ import calendar
 
 f  = open("drinking_answers.txt", "r")
 lines  = f.readlines()
-
+dic ={} #key: date; value
 for each in lines[1:]:
 	m = each.split(",")
 	gfgid = m[0]
@@ -14,4 +14,18 @@ for each in lines[1:]:
 	key = year + month + day;
 	print "key is ", key
 	print "year:",year,"month:", month, "day:",day,"had?:", had," #",num
+	if key not in dic:
+		dic[key] = {}
+		dic[key]["no"] = 0
+		dic[key]["yes"] = 0
+		dic[key]["numyes"] = 0
+		dic[key]["total"] = 0
+		if had == "No":
+			dic[key]["no"] = dic[key]["no"] + 1
+		elif had == "Yes":
+			dic[key]["yes"] = dic[key]["yes"] + 1
+			dic[key]["numyes"] = dic[key]["numyes"] + num
+		dic[key]["total"] = dic[key]["total"] + 1
 	break
+
+print dic
