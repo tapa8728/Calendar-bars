39fd4e01e34670741a76bf6de5326003374ce876
Tanvi Parikh <tapa8728@colorado.edu>
2016-04-29 16:25:33 -0600

    Drinking Time Series completed

--- operations.py
+++ operations.py
@@ -16,7 +16,7 @@ for each in lines[1:]:
 	num = m[7].replace("\n", "")
 	key = year +"-"+ "%02d" % int(month)+"-"+ "%02d" % int(day);
 	print "key is ", key
-	print "gender:",gender,"year:",year,"month:", month, "day:",day,"had?:", had," #",num
+	#print "gender:",gender,"year:",year,"month:", month, "day:",day,"had?:", had," #",num
 	if key not in dic:
 		dic[key] = {}
 		dic[key]["female"] = {}
@@ -65,14 +65,14 @@ for each in lines[1:]:
 				dic[key]["female"]["numyes"]+= int(num)
 			dic[key]["female"]["total"]+= 1
 
-	if dic[key]["female"]["total"] == 5:
-		break
+	# if dic[key]["female"]["total"] == 5:
+	# 	break
 
 print dic
 # with open('drinking.json', 'w') as outfile:
 #     json.dump(dic, outfile)
 
-# # list of dictionaries    
+# list of dictionaries    
 # drinkinglist =[] 
 # for k,v in dic.iteritems():
 # 	d = {}
@@ -85,3 +85,26 @@ print dic
 # drinkinglist.sort(key=operator.itemgetter('day'))
 # with open('Oct_drinking.json', 'w') as outfile2:
 #     json.dump(drinkinglist, outfile2)
+
+timeseries_list =[] 
+for k,v in dic.iteritems():
+	d = {} #empty dictionary
+	print "key :: ", k
+	print "value ::", v
+	d["date"] = k
+	if v["male"]["total"] == 0:	
+		d["male"] = 0
+	else:
+		d["male"] = round(v["male"]["numyes"]/float(v["male"]["total"]), 2)
+	if v["female"]["total"] == 0:	
+		d["female"] = 0
+	else:
+		d["female"] = round(v["female"]["numyes"]/float(v["female"]["total"]), 2)
+	timeseries_list.append(d)
+
+print timeseries_list
+
+
+timeseries_list.sort(key=operator.itemgetter('date'))
+with open('timeseries_data.json', 'w') as outfile3:
+    json.dump(timeseries_list, outfile3)
